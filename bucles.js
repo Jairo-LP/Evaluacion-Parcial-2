212da99ae9b73c5b76c5f3e9f0ccf2c93d9a6e58
Jairo <jairo15102001@gmail.com>
2026-07-24 08:16:04 -0500

    feat: creacion de bucles ejericicio 5

---
+++ bucles.js
@@ -0,0 +1,11 @@
+//bucles numeros del 1 hasta el 20 mostrando cuales son pares e impares
+
+
+for (let i = 0; i <= 20; i++){ 
+    if(i % 2 == 0){
+    console.log(`los numeros pares son: ${i}`);
+    }else{
+    console.log(`los numeros impares son ${i}`);
+    }
+
+}
