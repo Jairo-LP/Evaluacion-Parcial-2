f8d8ba6b104105a2891cfd0fdf489909c053a927
Jairo <jairo15102001@gmail.com>
2026-07-24 08:38:27 -0500

    fix: correciones de errores de bucles al contar pares e impares

--- bucles.js
+++ bucles.js
@@ -1,11 +1,17 @@
-//bucles numeros del 1 hasta el 20 mostrando cuales son pares e impares
+//bucles numeros del 1 hasta el 20 contando pares e impares
 
+let pares = 0;
+let impares = 0;
 
-for (let i = 0; i <= 20; i++){ 
-    if(i % 2 == 0){
-    console.log(`los numeros pares son: ${i}`);
-    }else{
-    console.log(`los numeros impares son ${i}`);
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(`los numeros pares son${i}`);
+        pares++;
+    } else {
+        console.log(`los numeros impares son${i}`);
+        impares++;
     }
-
 }
+
+console.log(`Numeros pares del 1 al 20: ${pares}`);
+console.log(`Numeros impares del 1 al 20: ${impares}`);
