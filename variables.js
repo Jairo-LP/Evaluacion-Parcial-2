a5c4422802f3257cee226f8cb73d5a1eac7c1cd3
Jairo <jairo15102001@gmail.com>
2026-07-24 07:45:43 -0500

    feat: creacion del documento variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+//declaracion de varibales que almacenen nombre, precio y disponibilidad en stock
+
+let nombre = "jairo";
+let precio1 = 9;
+let stock1 = 15;
+let precio2 = 8;
+let stock2 = 2;
+
+console.log(`Que tal ${nombre}`);
+
+console.log(`el precio del primer producto es: ${precio1} y el stock es de: ${stock1}`);
+console.log(`el precio del segundo producto es: ${precio2} y el stock es de: ${stock2}`);
