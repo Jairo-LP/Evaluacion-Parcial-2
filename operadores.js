08a0a9710c8b16bdca0ed59c681b041de376cc74
Jairo <jairo15102001@gmail.com>
2026-07-24 07:52:37 -0500

    feat: creacion del ejercicio 3 operadores

---
+++ operadores.js
@@ -0,0 +1,10 @@
+//calcular el precio final aplicando un descuento del 15%
+let preciofinal = 15;
+
+let ahorro = 15 * 0.15;
+
+let precio = preciofinal - ahorro;
+
+console.log(`el precio estandar es: ${preciofinal}`);
+console.log(`Su precio final es: ${precio}`);
+console.log(`Su ahorro fue de: ${ahorro}`);
