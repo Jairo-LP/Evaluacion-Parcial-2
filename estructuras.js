923e2e6366100bc3188c987d778404288c96345c
Jairo <jairo15102001@gmail.com>
2026-07-24 08:00:22 -0500

    feat: creacion de estructuras ejercicio 4 condicionales con edad

---
+++ estructuras.js
@@ -0,0 +1,9 @@
+let edad = 65;
+
+if (edad <= 17){
+    console.log(`Usted es menor de edad`);
+} else if (edad <=64){
+    console.log(`Usted es mayor de edad`);
+}else{
+    console.log(`Usted es de tercera edad`);
+}
